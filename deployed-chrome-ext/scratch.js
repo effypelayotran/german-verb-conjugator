f391691085bcaea6429530ca77e1a982c00d4dca
Effy Pelayo Tran <effy_pelayo_tran@brown.edu>
2023-07-21 13:16:08 -0700

    dkjfgn

---
+++ deployed-chrome-ext/scratch.js
@@ -0,0 +1,9 @@
+/* Scartch Paper Code */
+// let paragraphs = document.getElementsByTagName('p');
+// let giantText = '';
+// for (elt of paragraphs) {
+//   elt.style.backgroundColor = '#F4C2C2';
+//   giantText += elt.textContent + ' ';
+// }
+// console.log("giantText STARTS HERE")
+// console.log(giantText);
--- deployed-chrome-ext/script.js
+++ deployed-chrome-ext/script.js
@@ -1,18 +1,8 @@
 /* Save ALL text, this includes the ones that are in titles/menus/captions and 
 don't fall under the paragraph 'p' tag. */
-// let allText = document.body.innerText;
-// console.log(allText);
 
-
-/* Fetch word+translation+tense+ conjugation tables for each verb on the page*/
-// fetch("https://us-central1-marine-compass-391323.cloudfunctions.net/search-firestore-db?text=" + encodeURIComponent(allText))
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log(data.result); // Log the result key and value
-//     })
-//     .catch(error => {
-//         console.error(error); // Log any errors
-//     });
+let allText = document.body.innerText;
+console.log(allText);
 
 
 /* Save paragraph text on page */
@@ -21,12 +11,12 @@ let giantText = '';
 for (elt of paragraphs) {
   giantText += elt.textContent + ' ';
 }
-console.log("giantText STARTS HERE")
-console.log(giantText);
+// console.log("giantText STARTS HERE")
+// console.log(giantText);
 
 /* Fetch the list of verbs from the postagger-2 cloud function.
 THIS VERSION OF THE CODE WORKS. IT SUCCESSFULLY HIGHLIGHTS VERBS ON THE PAGE */
-fetch("https://us-central1-marine-compass-391323.cloudfunctions.net/postagger-2?text=" + encodeURIComponent(giantText))
+fetch("https://us-central1-marine-compass-391323.cloudfunctions.net/postagger-2?text=" + encodeURIComponent(allText))
     .then(response => response.json())
     .then(verbsData => {
         console.log(verbsData.result); // Log the list of verbs
@@ -35,7 +25,7 @@ fetch("https://us-central1-marine-compass-391323.cloudfunctions.net/postagger-2?
         const verbs = verbsData.result;
         verbs.forEach(verb => {
             const regex = new RegExp(`\\b${verb}\\b`, "gi");
-            const matches = giantText.match(regex);
+            const matches = allText.match(regex);
             if (matches) {
                 matches.forEach(match => {
                     document.body.innerHTML = document.body.innerHTML.replace(
@@ -49,16 +39,3 @@ fetch("https://us-central1-marine-compass-391323.cloudfunctions.net/postagger-2?
     .catch(error => {
         console.error(error); // Log any errors
     });
-
-
-
-/* Scartch Paper Code */
-// let paragraphs = document.getElementsByTagName('p');
-// let giantText = '';
-// for (elt of paragraphs) {
-//   elt.style.backgroundColor = '#F4C2C2';
-//   giantText += elt.textContent + ' ';
-// }
-// console.log("giantText STARTS HERE")
-// console.log(giantText);
-
