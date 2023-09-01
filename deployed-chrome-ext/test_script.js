e57ad9e8eb937ef912f927c7bf66d98f29efcb69
encryptioneffy <98721353+encryptioneffy@users.noreply.github.com>
2023-09-01 18:08:19 -0400

    Delete deployed-chrome-ext directory

--- deployed-chrome-ext/test_script.js
+++
@@ -1,9 +0,0 @@
-/* Test Frontend Code */
-// let paragraphs = document.getElementsByTagName('p');
-// let giantText = '';
-// for (elt of paragraphs) {
-//   elt.style.backgroundColor = '#F4C2C2';
-//   giantText += elt.textContent + ' ';
-// }
-// console.log("giantText STARTS HERE")
-// console.log(giantText);
